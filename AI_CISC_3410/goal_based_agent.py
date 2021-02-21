370f387cf8abc25df6bb8d652f3cf3613575467a
Adam Peavey <adam.peavey@disneystreaming.com>
2021-02-20 20:00:12 -0500

    Minor comment edits

--- AI_CISC_3410/goal_based_agent.py
+++ AI_CISC_3410/goal_based_agent.py
@@ -1,8 +1,8 @@
-# Initial state of farmer, fox, goose, grain
-    #   [farmer, fox, goose, grain]
+# Initial state, goal state
+    #   [farmer, fox, chicken, grain]
     #   1 == crossed the river
     #   0 == not crossed
-state = [0, 0, 0, 0]
+initial_state, goal_state = [0,0,0,0], [1,1,1,1]
 
 # States that break the rules, not allowed
 invalid_states = [[0, 1, 1, 1], \
@@ -12,19 +12,15 @@ invalid_states = [[0, 1, 1, 1], \
                   [0, 0, 1, 1], \
                   [1, 1, 0, 0]]
 
-# Final, desired state
-goal_state = [1,1,1,1]
-
 # Standard depth first search variables (iterative)
-discovered = []
-stack = [[0, 0, 0, 0]] # [0,0,0,0] is "root" of decision tree
+stack, discovered = [[intial_state]], []
 
 # Standard depth first search algorithm (iterative)
 while (stack):
     current_state = stack.pop()
     print("Current state : ", [int(x) for x in current_state])
 
-    # Checks if current state is final solution - solved!
+    # Checks if current state is goal - solved!
     if current_state == goal_state : break
     
     # standard DFS
@@ -35,7 +31,7 @@ while (stack):
         if current_state[0] == current_state[i]:
             test_state = current_state.copy()
 
-            # flip values
+            # flip values - "Cross the river"
             test_state[0], test_state[i] = not current_state[0], not current_state[i]
 
             # check if test_state is an allowed state
